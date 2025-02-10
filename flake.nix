a8b8eef68779c4e575611c84892ecdadad63b85f
chxry <floppa9@proton.me>
2025-02-10 20:15:38 +0000

    use pnpm

--- flake.nix
+++ flake.nix
@@ -3,22 +3,41 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
       forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
-    in {
-    packages = forAllSystems (system: {
-      default = with import nixpkgs { inherit system; }; pkgs.buildNpmPackage {
-        pname = "floppa site";
-        version = "1.0.0";
+    in
+    {
+      packages = forAllSystems (system: {
+        default =
+          with import nixpkgs { inherit system; };
+          let
+            manifest = builtins.fromJSON (builtins.readFile ./package.json);
+          in
+          pkgs.stdenv.mkDerivation (finalAttrs: {
+            pname = manifest.name;
+            version = manifest.version;
 
-        src = ./.;
-        npmDepsHash = "sha256-ojiUcsDdXGMageLjL98AUOvxrZJV4YUu/JJf955VX4o=";
-      
-        installPhase = ''
-          cp -pr dist $out
-        '';
-      };
-    });
-  };
+            src = ./.;
+            nativeBuildInputs = [ nodejs pnpm.configHook ];
+            pnpmDeps = pnpm.fetchDeps {
+              inherit (finalAttrs) pname version src;
+              hash = "sha256-fUTBIBVxOR8eBRVosYNqJN3pLwy1j4AV1uI27ZyJfKg=";
+            };
+
+            buildPhase = ''
+              runHook preBuild
+              pnpm run build
+              runHook postBuild
+            '';
+            installPhase = ''
+              runHook preInstall
+              mkdir -p $out
+              cp -r dist/. $out
+              runHook postInstall
+            '';
+          });
+      });
+    };
 }
